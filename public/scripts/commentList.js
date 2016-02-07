63a517a21c154862a26af3440861ce54630fcf6e
AyabongaQwabi <ayabonga@projectcodex.co>
2016-02-07 10:15:29 +0200

    materialized and added a story

---
+++ public/scripts/commentList.js
@@ -0,0 +1,16 @@
+var CommentList = React.createClass({render:function(){
+
+		return (
+			<div className='commentList'>
+				Hey there I am a CommentList
+			</div>
+		)
+}})
+
+var CommentForm = React.createClass({render:function(){
+	return (
+		<div className='commentForm'>
+			Hey I am a CommentForm 
+		</div>
+	)
+}})
