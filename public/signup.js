5cbcfd02e68777d9ebe7fbf1be942200df0139f5
AyabongaQwabi <ayabonga@projectcodex.co>
2016-02-08 10:19:36 +0200

    signup form created

--- public/signup.js
+++ public/signup.js
@@ -1,5 +1,5 @@
 var SignupInfo = React.createClass({
-		getInitialState:function(){
+		whenLoad:function(){
 			return ({signupStory:''})
 		},
 		fetchSignUpStory:function(){
@@ -18,4 +18,38 @@ var SignupInfo = React.createClass({
 				</div>
 			)
 		}
+})
+
+var SignupForm = React.createClass({
+	whenLoad:function(){
+		this.setState({name:'',email:''})
+	},
+	getName:function(e){
+		this.setState({name:e.target})
+	},
+	getEmail:function(e){
+		this.setState({email:e.target})
+	},
+	sendForm:function(){
+		var details ={
+			name:this.state.name.value,
+			email:this.state.email.value
+		}
+		if(!details.name || !details.email){
+			return;
+		}
+		this.props.submitForm(details)
+		this.state.name.value='';
+		this.state.email.value='';
+	},
+	render:function(){
+		return (
+			<form>
+				<input type='text' onChange={this.getName} placeholder='your Name' className='required validate'>
+				<input type='text' onChange={this.getEmail} placeholder='your email' className='required validate'>
+				<input type='submit' value='Send' className='btn-large white' onSubmit={this.sendForm}>
+			</form>
+
+		)
+	}
 })
